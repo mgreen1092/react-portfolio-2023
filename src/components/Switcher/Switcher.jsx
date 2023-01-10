47dbd05491536b3d3ace2caaab70acc456513fd6
mgreen1092 <morgan.green0209@gmail.com>
2023-01-10 09:45:28 -0500

    switcher file added with states and toggleDarkMode

---
+++ src/components/Switcher/Switcher.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useState } from "react";
+import { DarkModeSwitch } from "react-toggle-dark-mode";
+import useDarkSide from "../hooks/useDarkSide";
+
+const Switcher = () => {
+    const [colorTheme, setTheme] = useDarkSide();
+    const [darkSide, setDarkSide] = useState(
+        colorTheme === "light" ? true : false
+    );
+  
+    const toggleDarkMode = (checked) => {
+        setTheme(colorTheme);
+        setDarkSide(checked);
+    };
+  
+    return (
+        <>
+            <DarkModeSwitch
+                style={{ marginBottom: "2rem" }}
+                checked={darkSide}
+                onChange={toggleDarkMode}
+                size={30}
+            />
+        </>
+    );
+}
+
+export default Switcher
