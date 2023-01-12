cde6054b3ea65cd075d22d1377705e970501490b
mgreen1092 <morgan.green0209@gmail.com>
2023-01-12 10:04:50 -0500

    white text logo appears but wrong size

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,7 +6,7 @@ import Logo from '../../assets/logo.svg'
 import {Link} from 'react-scroll'
 import { useState } from "react";
 import Switcher from '../Switcher/Switcher'
-import DarkModeLogo from '../../assets/logo.svg'
+import DarkModeLogo from '../../assets/darkModeLogo.svg'
 
 export default function Nav () {
     const [nav, setNav] = useState(false)
