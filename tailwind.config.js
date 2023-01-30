439fa8a41de0f4b7ec03eaaed4b400750fab33a0
mgreen1092 <morgan.green0209@gmail.com>
2023-01-30 15:15:00 -0500

    added google analytic tag

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
       'sm': '640px',
       // => @media (min-width: 640px) { ... }
 
-      'md': '768px',
+      'md': '750px',
       // => @media (min-width: 768px) { ... }
 
       'lg': '1024px',
